27c681a7b0a9c8a8164ec9f33d554ca963731619
Thomas Moran <thomasmoran@Thomass-MacBook-Pro.local>
2021-09-29 17:12:23 +0100

    Cleaned up input and errors and removing and general tidy up after testing

--- src/pycachu/cache.py
+++ src/pycachu/cache.py
@@ -3,7 +3,9 @@
 import os
 import keyring
 import yaml
+from getpass import getpass
 
+import errors
 
 def _generate_keyring_key(file_location, key):
     file_key_part = file_location.replace("/", "_")
@@ -11,27 +13,42 @@ def _generate_keyring_key(file_location, key):
     keyring_key = f"{file_key_part}.{key}"
     return keyring_key
 
+def get_input(prompt, hide=False):
+    if hide == True:
+        return getpass(prompt)
+    elif hide == False:
+        return input(prompt)
+    else:
+        raise SyntaxError("get_input: hide paramter must be True or False")
+
 
 class Token:
-    def _get_token_from_user(self, prompt=None):
-        if prompt is None:
-            return input(
-                f"Please input the token for {self.key} in the {self.cache.file_name} cache: "
+    def _get_token_from_user(self):
+        if self.user_prompt is None:
+            return get_input(
+                f"Please input the token for {self.key} in the {self.cache.file_name} cache: ", hide=self.hide_input
             )
         else:
-            return input(prompt)
+            return get_input(self.user_prompt, hide=self.hide_input)
 
-    def __init__(self, cache, key, user_prompt=None):
+    def __init__(self, cache, key, user_prompt=None, hide_input=False):
         self.cache = cache
         self.key = key
+        self.user_prompt = user_prompt
+        self.hide_input = hide_input
         if key not in cache.tokens:
-            self.token = self._get_token_from_user(prompt=user_prompt)
+            self.token = self._get_token_from_user()
             self.cache.tokens[key] = self.token
             self.cache._write()
         else:
             self.token = cache.tokens[key]
 
-    def reset(self, new_token):
+    def reset(self, new_token=None, user_prompt=None):
+        if user_prompt is not None:
+            self.user_prompt = user_prompt
+        if new_token == None:
+            new_token = self._get_token_from_user()
+        self.token = new_token
         self.cache.tokens[self.key] = new_token
         self.cache._write()
 
@@ -39,30 +56,36 @@ class Token:
         del self.cache.tokens[self.key]
         self.cache._write()
 
+    def error(self, **kwargs):
+        raise errors.CacheTokenError(self, **kwargs)
 
 class Credential:
-    def _get_username_from_user(self, prompt=None):
-        if prompt is None:
-            return input(
-                f"Please input the username for {self.key} in the {self.cache.file_name} cache: "
+    def _get_username_from_user(self):
+        if self.username_prompt is None:
+            return get_input(
+                f"Please input the username for {self.key} in the {self.cache.file_name} cache: ", hide=self.hide_username
             )
         else:
-            return input(prompt)
+            return get_input(self.username_prompt, hide=self.hide_username)
 
-    def _get_password_from_user(self, prompt=None):
-        if prompt is None:
-            return input(
-                f"Please input the password for {self.key} in the {self.cache.file_name} cache: "
+    def _get_password_from_user(self):
+        if self.password_prompt is None:
+            return get_input(
+                f"Please input the password for {self.key} in the {self.cache.file_name} cache: ", hide=self.hide_password
             )
         else:
-            return input(prompt)
+            return get_input(self.password_prompt, hide=self.hide_password)
 
-    def __init__(self, cache, key, username_prompt=None, password_prompt=None):
+    def __init__(self, cache, key, username_prompt=None, password_prompt=None, hide_username=False, hide_password=True):
         self.cache = cache
         self.key = key
+        self.username_prompt = username_prompt
+        self.password_prompt = password_prompt
+        self.hide_username = hide_username
+        self.hide_password = hide_password
         self.keyring_key = _generate_keyring_key(cache.file_location, key)
         if key not in cache.credentials:
-            self.username = self._get_username_from_user(prompt=username_prompt)
+            self.username = self._get_username_from_user()
             self.cache.credentials[key] = self.username
             self.cache._write()
         else:
@@ -70,19 +93,25 @@ class Credential:
 
         self.password = keyring.get_password(self.keyring_key, self.username)
         if self.password is None:
-            self.password = self._get_password_from_user(prompt=password_prompt)
+            self.password = self._get_password_from_user()
             keyring.set_password(self.keyring_key, self.username, self.password)
 
     def reset_password(self, password_prompt=None):
-        self.password = self._get_password_from_user(prompt=password_prompt)
+        if password_prompt is not None:
+            self.password_prompt = password_prompt
+        self.password = self._get_password_from_user()
         keyring.set_password(self.keyring_key, self.username, self.password)
 
     def reset(self, username_prompt=None, password_prompt=None):
+        if username_prompt is not None:
+            self.username_prompt = username_prompt
+        if password_prompt is not None:
+            self.password_prompt = password_prompt
         keyring.delete_password(self.keyring_key, self.username)
         self.username = self._get_username_from_user(prompt=username_prompt)
         self.cache.credentials[self.key] = self.username
         self.cache._write()
-        self.password = self._get_password_from_user(prompt=password_prompt)
+        self.password = self._get_password_from_user()
         keyring.set_password(self.keyring_key, self.username, self.password)
 
     def remove_password(self):
@@ -93,30 +122,30 @@ class Credential:
         del self.cache.credentials[self.key]
         self.cache._write()
 
+    def error(self, **kwargs):
+        raise errors.CacheCredentialError(self, **kwargs)
+
 
 class Cache:
     def __init__(self, file_name, path="", reset=False):
         self.file_name = file_name
         self.file_path = path
-        self.file_location = f"{path}/{file_name}"
+        self.file_location = f"{path}{file_name}"
 
-        if reset == True and os.path.isfile(self.file_location):
-            self.file = open(self.file_location, "w")
+        if reset == True and os.path.isfile(self.file_location): # file exists, but we're resetting it
             self._load()
             self.clear()
             self._create()
-        elif os.path.isfile(self.file_location):
-            self.file = open(self.file_location, "w")
+        elif os.path.isfile(self.file_location) and os.path.getsize(self.file_location) == 0: # file exists, but is empty
+            self._create()
+        elif os.path.isfile(self.file_location): # file exists and is good to be loaded up
             self._load()
-        else:
-            self.file = open(self.file_location, "w")
+        else: # file does not exist
             self._create()
 
-    def __del__(self):
-        self.file.close()
-
     def _write(self):
-        yaml.dump(self.full_dict, self.file, default_flow_style=False)
+        with open(self.file_location, "w") as cache_file:
+            yaml.dump(self.full_dict, cache_file, default_flow_style=False)
 
     def _create(self):
         self.tokens = {}
@@ -126,41 +155,44 @@ class Cache:
 
     def _validate_base_key(self, key):
         if key not in self.full_dict:
-            raise Exception()  # This will get replaced with a homemade error
+            raise KeyError(f"Did not find {key} in the YAML from {self.file_name}")
         if not isinstance(self.full_dict[key], dict):
-            raise Exception()  # This will get replaced with a homemade error
+            raise TypeError(f"{key} in the YAML from {self.file_name} is not in the expected key-value format")
 
-    def _validate(self):
+    def _validate_dict(self):
         if not hasattr(self, "full_dict"):
-            raise Exception()  # This will get replaced with a homemade error
+            raise errors.CacheLoadError(f"No YAML was loaded from {self.file_name}")
+        if not isinstance(self.full_dict, dict):
+            raise errors.CacheLoadError(f"The YAML from {self.file_name} was not in the expected key-value format")   
         self._validate_base_key("tokens")
         self._validate_base_key("credentials")
 
     def _load(self):
-        self.full_dict = yaml.safe_load(self.file)
-        self._validate()
+        with open(self.file_location, "r") as cache_file:
+            try:
+                self.full_dict = yaml.safe_load(cache_file)
+            except:
+                raise errors.CacheLoadError(f"Valid YAML could not be safe-loaded from {self.file_name}")
+        self._validate_dict()
         self.tokens = self.full_dict["tokens"]
         self.credentials = self.full_dict["credentials"]
         self.full_dict = {"tokens": self.tokens, "credentials": self.credentials}
 
-    def get_token(self, key, user_prompt=None):
-        return Token(self, key, user_prompt=user_prompt)
+    def get_token(self, key, **kwargs):
+        return Token(self, key, **kwargs)
 
-    def get_credential(self, key, username_prompt=None, password_prompt=None):
-        return Credential(
-            self, key, username_prompt=username_prompt, password_prompt=password_prompt
-        )
+    def get_credential(self, key, **kwargs):
+        return Credential(self, key, **kwargs)
 
     def clear_credentials(self):
         for key in self.credentials:
-            keyring.delete_password(
-                _generate_keyring_key(self.file_location, key), self.credentials[key]
-            )
-        self.credentials = {}
+            if keyring.get_password(_generate_keyring_key(self.file_location, key), self.credentials[key]) != None:
+                keyring.delete_password(_generate_keyring_key(self.file_location, key), self.credentials[key])
+        self.credentials.clear()
         self._write()
 
     def clear_tokens(self):
-        self.tokens = {}
+        self.tokens.clear()
         self._write()
 
     def clear(self):
